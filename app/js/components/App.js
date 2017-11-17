4fdb08530aac9768da374450cf1eb90ea665b2fe
joe burton <joeburton@gmail.com>
2017-11-17 15:32:06 +0000

    imbeded react component

--- app/js/components/App.js
+++ app/js/components/App.js
@@ -4,16 +4,30 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import Navigation from './Navigation'
+import Content from './content'
 
 // Containers
 import AddProject from '../containers/AddProject'
 import VisibleProjectList from '../containers/VisibleProjectList'
+
+let items = [{
+  description: 'Transfer from EUR',
+  conversionRate: 1,
+  transactionCurrency: 'EUR',
+  billAmount:  1.3
+}, {
+  description: 'Transfer from GBP',
+  conversionRate: 2,
+  transactionCurrency: 'GBP',
+  billAmount:  1.1
+}];
 
 const App = () => (
   <div>
     <Navigation />
     <AddProject />
     <VisibleProjectList />
+    <Content items={items} />
     <Footer />
   </div>
 )
--- app/js/components/Content.js
+++ app/js/components/Content.js
@@ -4,10 +4,30 @@ export default class Content extends React.Component {
     constructor(props) {
         super(props);
     }
-    render() {
+   render() {
+        console.log(this.props)
+
+        let transactionFrom = [];
+        let billAmount = [];
+    
+        for (let i = 0; i < this.props.items.length; i++) {
+            
+            let item = '';
+    
+            if (this.props.items[i].conversionRate === 1) {
+                transactionFrom.push(<span className="copy-reference text-left" key={i}>{this.props.items[i].description}<br/></span>);
+            } else {
+                transactionFrom.push(<span className="copy-reference text-left" key={i}>{this.props.items[i].description} at {this.props.items[i].conversionRate}</span>)
+            }
+
+            billAmount.push(<p className="copy-total" key={i}>{this.props.items[i].transactionCurrency + this.props.items[i].billAmount}</p>);
+
+        }
         return (
             <section className="content">
                 <h1>React Motion - Navigation</h1>
+                {transactionFrom}
+                {billAmount}
             </section>
         );
     }
